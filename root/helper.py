bc9669c4072a973b3b0e056d89f3fd50bc7f916d
batikfatigue <justinngjr@gmail.com>
2024-05-19 17:18:08 +0800

    bit of work on category processing
    didnt work out, hit api requests limits
    todo:
    - get request processing sorted out
    - get notes sorted out
    - for requests: if misc, all of the misc categories will have to be verified by the user
    - for notes: if there's a category, gpt will provide a very short note. if no cat, users will enter notes by themselves, can also choose not to

--- root/helper.py
+++ root/helper.py
@@ -15,7 +15,7 @@ def reformatter(text_stream, categories):
 
     with text_stream as file:
         data = csv.reader(file)
-        i = 0
+        i = 1
         for row in data:
             if row == []:
                 continue
@@ -32,15 +32,59 @@ def reformatter(text_stream, categories):
                     amount = row[2][1:]
                     type1 = 'Expense'
                 
-                category = categoriser(categories, row[4:7])
+            
+                # this is repeated , maybe can use function
+                # if row[1] == 'INT':
+                #     note = 'Interest Earned'
+                #     category = 'Interest'
+                #     newRow = [date, note, amount, type1, category]
+
+ 
+                #     f = open(csv_path, 'a', newline='')
+                #     writer = csv.writer(f)
+                #     writer.writerow(newRow)
+                #     print(f'Row{i} done for {reference}')
+                #     i += 1
+
+                #     continue
+                    
+                    
+                
+                # Processing references
+                # if nets qr then find the TO: 
+                if row[1] == 'MST':
+                    reference = row[5]
+                else: 
+                    reference = row[4:7]
+                    dup = [k for k in reference]
+                    found = False
+                    for j, ref in enumerate(dup):
+                        print(ref)
+                        if 'PayNow Transfer' in ref:
+                            reference.remove(dup[j])
+
+                        if 'OTHR' in ref:
+                            ref = ref[5:]
+                            misc = categoriser(categories, ref)
+                            if 'Miscellaneous' in misc:
+                                reference.remove(dup[j])
+                            else:
+                                category = misc
+                                found = True
+                                break
+                        print(
+                            'Checking: {}'.format(reference)
+                        )
+
+                print(reference)
+                if not found:
+                    category = categoriser(categories, reference)
                 print(category)
 
-                if row[1] == 'INT':
-                    note = 'Interest Earned'
-
-                
+                # we will let the user manually type in categories that chatgpt sort as misc, less they want to sort it as misc
 
                 newRow = [date, note, amount, type1, category]
+
 
                 f = open(csv_path, 'a', newline='')
                 writer = csv.writer(f)
@@ -65,9 +109,15 @@ def categoriser(categories, reference):
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": """You are an analyser.
-                                        Choose a category based off 3 references, for money tracking. A single word.
-                                        Categories you choose from: {}.""".format(categories)},
+        {"role": "system", "content": """You are an bank transactions analyser.
+                                        Categories: {}.
+                                        You pick a most suitable category from the above, according to the given transaction references.
+                                        You don't look at patterns, you focus on the names/brands.
+                                        You resort to Miscellaneous whenever there's little context.
+                                        You explain why you chose so, in a short sentence.
+                                        
+                                        """.format(categories)},
+
         {"role": "user", "content": reference}
     ]
     )
